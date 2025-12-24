53b3cf96a835974603ad00805240366872a67351
Emirhan <emirhanerkn@hotmail.com>
2025-12-24 16:28:54 +0300

    feat(page): add useHeader context in topics page and main page for now for dynamic header text. add StatCard component to mainpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+"use client"
 import Image from "next/image";
 import Sidebar from "@/components/layout/Sidebar";
+import StatCard from "@/components/dashboard/StatCard";
+import {useHeader} from "@/app/providers";
+import {useEffect} from "react";
 export default function Home() {
+    const {setHeaderText} = useHeader();
+    useEffect(()=> {
+        setHeaderText("Track your progress and stay focused")
+    })
   return (
-     <Sidebar/>
+      <div className="px-20 py-8">
+    <StatCard/>
+      </div>
   );
 }
--- src/app/topics/page.tsx
+++ src/app/topics/page.tsx
@@ -1,5 +1,11 @@
-
+"use client"
+import {useHeader} from "@/app/providers";
+import {useEffect} from "react";
 const TopicsPage = () => {
+    const {setHeaderText} = useHeader()
+    useEffect(()=>{
+        setHeaderText("Manage all your learning topics")
+    })
     return (
         <div>
             emirhan
