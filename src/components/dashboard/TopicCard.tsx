32787c44905107dea89e9fc95e16b0794e86f617
Emirhan <emirhanerkn@hotmail.com>
2026-01-12 17:51:09 +0300

    feat: add shadow to the topiccards and add icon

--- src/components/dashboard/TopicCard.tsx
+++ src/components/dashboard/TopicCard.tsx
@@ -2,7 +2,7 @@ import {topicCards,levelStyles} from "@/types/topicCard";
 import { Card, CardContent, CardDescription,CardTitle } from "@/components/ui/card"
 import {Progress} from "@/components/ui/progress";
 import {useEffect, useState} from "react";
-
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 const TopicCard = () => {
     const [progress, setProgress] = useState(13)
 
@@ -16,7 +16,7 @@ const TopicCard = () => {
             {topicCards.map((prop, key) => {
                 const style = levelStyles[prop.level];
                 return (
-                    <Card key={key} className="w-full min-h-[185px] p-5">
+                    <Card key={key} className="w-full hover:shadow-md min-h-[185px] p-5">
                         <CardContent>
                             <div className="flex flex-col items-start gap-3">
                                 <CardTitle>{prop.title}</CardTitle>
@@ -25,11 +25,13 @@ const TopicCard = () => {
                                     className={`px-3 py-1 rounded-md text-xs font-medium ${style.bg} ${style.text} ${style.border}`}>
                                     {prop.level}
                                 </div>
-                                <div className="space-y-2">
-                                    <Progress value={progress} className="w-full h-1 sm:h-3"/>
+                                    <Progress value={progress} className="w-full"/>
                                     <p className="text-xs sm:text-sm text-gray-500 text-right">
                                         {progress}%
                                     </p>
+                                <div className="flex gap-2">
+                                <CalendarTodayIcon fontSize={"inherit"}/>
+                                <span className="text-[#a1a1a1] text-[12px]">Last Studied: Today</span>
                                 </div>
                             </div>
                         </CardContent>
