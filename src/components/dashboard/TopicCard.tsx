d4fc0a3c20fda3b8d57fdf4190e1623bd11598d3
Emirhan <emirhanerkn@hotmail.com>
2026-01-13 14:05:40 +0300

    feat: add TopicCardProps and use as props. add level and statusstyles to create dynamic color

--- src/components/dashboard/TopicCard.tsx
+++ src/components/dashboard/TopicCard.tsx
@@ -1,29 +1,39 @@
-import {topicCards,levelStyles} from "@/types/topicCard";
+import {TopicCards} from "@/types/topicCard";
 import { Card, CardContent, CardDescription,CardTitle } from "@/components/ui/card"
 import {Progress} from "@/components/ui/progress";
 import {useEffect, useState} from "react";
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-const TopicCard = () => {
+import {levelStyles} from "@/types/topicCard";
+import {any} from "prop-types";
+import {LevelType} from "@/types/topicCard";
+import {statusStyles} from "@/types/topicCard";
+import {Status} from "@/types/topicCard";
+interface TopicCardProps {
+    title: string;
+    category: string;
+    level: LevelType;
+    message?: string;
+    status: Status;
+}
+
+const TopicCard = ({title,category,level,status}: TopicCardProps) => {
     const [progress, setProgress] = useState(13)
 
     useEffect(() => {
         const timer = setTimeout(() => setProgress(66), 500)
         return () => clearTimeout(timer)
     }, [])
-
+    const style = levelStyles[level]
+    const statusStyle = statusStyles[status]
     return (
-        <div className="w-full grid grid-cols-1 mt-4 sm:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-5">
-            {topicCards.map((prop, key) => {
-                const style = levelStyles[prop.level];
-                return (
-                    <Card key={key} className="w-full hover:shadow-md min-h-[185px] p-5">
+                    <Card className="w-full hover:shadow-md min-h-[185px] p-5">
                         <CardContent>
-                            <div className="flex flex-col items-start gap-3">
-                                <CardTitle>{prop.title}</CardTitle>
-                                <CardDescription>{prop.description}</CardDescription>
+                            <div className="flex flex-col relative items-start gap-3">
+                                <CardTitle className="font-normal">{title}</CardTitle>
+                                <CardDescription className="text-[#737373]">{category}</CardDescription>
                                 <div
-                                    className={`px-3 py-1 rounded-md text-xs font-medium ${style.bg} ${style.text} ${style.border}`}>
-                                    {prop.level}
+                                    className={`px-3 mb-1 py-1 rounded-md text-xs font-medium ${style?.bg} ${style?.text} ${style?.border}`}>
+                                    {level}
                                 </div>
                                     <Progress value={progress} className="w-full"/>
                                     <p className="text-xs sm:text-sm text-gray-500 text-right">
@@ -33,12 +43,12 @@ const TopicCard = () => {
                                 <CalendarTodayIcon fontSize={"inherit"}/>
                                 <span className="text-[#a1a1a1] text-[12px]">Last Studied: Today</span>
                                 </div>
+                                <div className={`px-3 py-1 absolute top-0 right-0 rounded-md text-xs font-medium ${statusStyle?.bg} ${statusStyle?.text} ${statusStyle?.border}`}>
+                                    {status}
+                                </div>
                             </div>
                         </CardContent>
                     </Card>
-                )
-            })}
-        </div>
     )
 }
 export default TopicCard;
