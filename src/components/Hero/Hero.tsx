fdb3f6f0cb1fcb820e2c7500231a6b613ec37bfc
Emirhan <emirhanerkn@hotmail.com>
2026-01-13 21:18:06 +0300

    fix: change font text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = ({title,description}: Hero) => {
     return (
         <div className="w-full mt-6 sm:mt-8 flex flex-col sm:flex-row gap-3 sm:gap-0 justify-between items-start sm:items-center">
             <div className="flex flex-col gap-1">
-                <h1 className="text-lg sm:text-xl text-[#171717] font-semibold">{title}</h1>
+                <h1 className="text-lg sm:text-xl text-[#171717] font-medium">{title}</h1>
                 <span className="text-xs sm:text-sm text-[#737373]">{description}</span>
             </div>
             <AddTopic />
