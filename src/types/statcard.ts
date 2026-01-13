f747cb01682716c6453d49d48cc9f888d67d621d
Emirhan <emirhanerkn@hotmail.com>
2026-01-13 14:02:18 +0300

    feat: add topicCards informations in statcard.ts

--- src/types/statcard.ts
+++ src/types/statcard.ts
@@ -2,25 +2,31 @@ import TopicRoundedIcon from '@mui/icons-material/TopicRounded';
 import PriorityHighRoundedIcon from '@mui/icons-material/PriorityHighRounded';
 import TrendingUpRoundedIcon from '@mui/icons-material/TrendingUpRounded';
 import BlurCircularRoundedIcon from '@mui/icons-material/BlurCircularRounded';
-import circle from "../../public/icons/circle.svg"
-export type Statcard = {
+
+export type StatCard = {
     title: string,
     icon: any,
 }
 
-export const statCardProps: Statcard[] = [
+export const activeTopics: StatCard[] = [
     {
         title: "Active Topics",
         icon: TopicRoundedIcon
     },
+]
+export const stuckTopics: StatCard[] = [
     {
-      title: "Stuck Topics",
+        title: "Stuck Topics",
         icon: PriorityHighRoundedIcon,
     },
+]
+export const weeklyProgress: StatCard[] = [
     {
         title: "Weekly Progress",
         icon: TrendingUpRoundedIcon
     },
+]
+export const todayFocus: StatCard[] = [
     {
         title: "Today's Focus",
         icon: BlurCircularRoundedIcon
