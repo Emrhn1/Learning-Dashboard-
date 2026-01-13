84eab42ff0fc44f8ea51f8167cb04c300a24333a
Emirhan <emirhanerkn@hotmail.com>
2026-01-13 14:03:37 +0300

    feat: add stuckTopicCount and activeTopicCount values by useAppSelector then use values to in cards

--- src/components/dashboard/StatCard.tsx
+++ src/components/dashboard/StatCard.tsx
@@ -1,12 +1,17 @@
-import { statCardProps } from "@/types/statcard";
+import { activeTopics, weeklyProgress, stuckTopics, todayFocus } from "@/types/statcard";
 import { Card, CardContent, CardDescription } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
-import { buttons } from "@/types/sidebarButton";
+import { useAppSelector } from "@/app/hooks";
+import {selectActiveTopicsCount,selectStuckTopicsCount} from "@/redux/features/topics/addTopics";
 
 const StatCard = () => {
+
+    const stuckCount = useAppSelector(selectStuckTopicsCount);
+    const activeCount = useAppSelector(selectActiveTopicsCount);
+
     return (
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 sm:gap-6">
-            {statCardProps.map((prop, key) => {
+            {activeTopics.map((prop, key) => {
                 const Icon = prop.icon
                 return (
                     <Card key={key} className="w-full hover:shadow-md min-h-[134px] sm:min-h-[170px] pt-4 sm:pt-6 pb-1">
@@ -14,7 +19,7 @@ const StatCard = () => {
                             <div className="flex flex-row justify-between">
                                 <div className="flex flex-col items-start gap-1">
                                     <Label className="text-[#737373] text-xs sm:text-sm">{prop.title}</Label>
-                                    <Label className="text-[#171717] text-2xl sm:text-3xl">4</Label>
+                                    <Label className="text-[#171717] text-2xl sm:text-3xl">{activeCount}</Label>
                                     <Label className="text-[#a1a1a1] text-xs mt-8">+2 this week</Label>
                                 </div>
                                 <Icon className="text-[#737373] w-5 h-5 sm:w-6 sm:h-6">{prop.icon}</Icon>
@@ -23,6 +28,58 @@ const StatCard = () => {
                     </Card>
                 )
             })}
+            {stuckTopics.map((prop, key) => {
+                const Icon = prop.icon
+                return (
+                    <Card key={key} className="w-full hover:shadow-md min-h-[134px] sm:min-h-[170px] pt-4 sm:pt-6 pb-1">
+                        <CardContent>
+                            <div className="flex flex-row justify-between">
+                                <div className="flex flex-col items-start gap-1">
+                                    <Label className="text-[#737373] text-xs sm:text-sm">{prop.title}</Label>
+                                    <Label className="text-[#171717] text-2xl sm:text-3xl">{stuckCount}</Label>
+                                    <Label className="text-[#a1a1a1] text-xs mt-8">+2 this week</Label>
+                                </div>
+                                <Icon className="text-[#737373] w-5 h-5 sm:w-6 sm:h-6">{prop.icon}</Icon>
+                            </div>
+                        </CardContent>
+                    </Card>
+                )
+            })}
+            {weeklyProgress.map((prop, key) => {
+                const Icon = prop.icon
+                return (
+                    <Card key={key} className="w-full hover:shadow-md min-h-[134px] sm:min-h-[170px] pt-4 sm:pt-6 pb-1">
+                        <CardContent>
+                            <div className="flex flex-row justify-between">
+                                <div className="flex flex-col items-start gap-1">
+                                    <Label className="text-[#737373] text-xs sm:text-sm">{prop.title}</Label>
+                                    <Label className="text-[#171717] text-2xl sm:text-3xl">12 Hours</Label>
+                                    <Label className="text-[#a1a1a1] text-xs mt-8">+2 this week</Label>
+                                </div>
+                                <Icon className="text-[#737373] w-5 h-5 sm:w-6 sm:h-6">{prop.icon}</Icon>
+                            </div>
+                        </CardContent>
+                    </Card>
+                )
+            })}
+            {todayFocus.map((prop, key) => {
+                const Icon = prop.icon
+                return (
+                    <Card key={key} className="w-full hover:shadow-md min-h-[134px] sm:min-h-[170px] pt-4 sm:pt-6 pb-1">
+                        <CardContent>
+                            <div className="flex flex-row justify-between">
+                                <div className="flex flex-col items-start gap-1">
+                                    <Label className="text-[#737373] text-xs sm:text-sm">{prop.title}</Label>
+                                    <Label className="text-[#171717] text-2xl sm:text-3xl">3</Label>
+                                    <Label className="text-[#a1a1a1] text-xs mt-8">+2 this week</Label>
+                                </div>
+                                <Icon className="text-[#737373] w-5 h-5 sm:w-6 sm:h-6">{prop.icon}</Icon>
+                            </div>
+                        </CardContent>
+                    </Card>
+                )
+            })}
+
         </div>
     )
 }
