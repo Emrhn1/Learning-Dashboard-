0048952f8b487d2bdbdae83373ff8bfbab36f57b
Emirhan <emirhanerkn@hotmail.com>
2026-01-14 20:24:57 +0300

    feat: add useAppDispatch and use the updateFilterTopic reducer in the ToggleGroupItem by level and status

--- src/components/topics/TopicFilter.tsx
+++ src/components/topics/TopicFilter.tsx
@@ -1,8 +1,12 @@
-import { Card, CardContent, CardDescription,CardTitle } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card"
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
 import {ToggleGroup,ToggleGroupItem} from "@/components/ui/toggle-group";
+import { useAppSelector,useAppDispatch } from "@/app/hooks";
+import { updateFilterTopic } from "@/redux/features/topics/addTopics";
 
 const TopicFilter = () => {
+
+    const dispatch = useAppDispatch();
     return (
         <Card className="w-full min-h-[161px] hover:shadow-md pt-[25px] pr-[25px] pb-0.5">
             <CardContent>
@@ -15,17 +19,28 @@ const TopicFilter = () => {
                         <h2 className="text-[#525252]">Difficulty Level</h2>
                         <ToggleGroup type={"single"} className="flex flex-wrap gap-2">
                             <ToggleGroupItem
+                                 defaultValue={"All Levels"}
+                                onClick={()=> dispatch(updateFilterTopic({level: "All Levels"}))}
                                 className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white"
                                 value="All Levels">
                                 All Levels
-                            </ToggleGroupItem>
-                            <ToggleGroupItem className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white" value="Beginner">
+                            </ToggleGroupItem>                    
+                            <ToggleGroupItem
+                                onClick={()=> dispatch(updateFilterTopic({level: "Beginner"}))}
+                                className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white"
+                                value="Beginner">
                                 Beginner
                             </ToggleGroupItem>
-                            <ToggleGroupItem className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white" value="Intermediate">
+                            <ToggleGroupItem
+                                onClick={()=> dispatch(updateFilterTopic({level: "Intermediate"}))}
+                                className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white"
+                                value="Intermediate">
                                 Intermediate
                             </ToggleGroupItem>
-                            <ToggleGroupItem className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white" value="Advanced">
+                            <ToggleGroupItem
+                                onClick={()=> dispatch(updateFilterTopic({level: "Advanced"}))}
+                                className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white"
+                                value="Advanced">
                                 Advanced
                         </ToggleGroupItem>
                         </ToggleGroup>
@@ -33,10 +48,10 @@ const TopicFilter = () => {
                     <div className="flex flex-col gap-4 mb-3 md:mb-2 sm:mb-4">
                         <h2 className="text-[#525252]">Status</h2>
                         <ToggleGroup type={"single"} className="flex flex-wrap gap-2">
-                            <ToggleGroupItem value={"All Status"} className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white">All Status</ToggleGroupItem>
-                            <ToggleGroupItem className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white" value={"Active"}>Active</ToggleGroupItem>
-                            <ToggleGroupItem className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white" value={"Stuck"}>Stuck</ToggleGroupItem>
-                            <ToggleGroupItem className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white" value={"Completed"}>Completed</ToggleGroupItem>
+                            <ToggleGroupItem defaultValue={"All Status"} value={"All Status"} onClick={()=> dispatch(updateFilterTopic({status:"All Status"}))} className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white">All Status</ToggleGroupItem>
+                            <ToggleGroupItem className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white"  value={"Active"} onClick={()=> dispatch(updateFilterTopic({status:"Active"}))}>Active</ToggleGroupItem>
+                            <ToggleGroupItem className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white" onClick={()=> dispatch(updateFilterTopic({status:"Stuck"}))} value={"Stuck"}>Stuck</ToggleGroupItem>
+                            <ToggleGroupItem className="text-[#171717] border !rounded-lg data-[state=on]:bg-[#171717] data-[state=on]:text-white" onClick={()=> dispatch(updateFilterTopic({status:"Completed"}))} value={"Completed"}>Completed</ToggleGroupItem>
                         </ToggleGroup>
                     </div>
                 </div>
