0a4c18c7b4238590022b287883cc7deee444830d
Emirhan <emirhanerkn@hotmail.com>
2026-01-14 20:25:34 +0300

    feat: show the topics by filter

--- src/app/topics/page.tsx
+++ src/app/topics/page.tsx
@@ -5,18 +5,19 @@ import TopicFilter from "@/components/topics/TopicFilter";
 import Hero from "@/components/Hero/Hero"
 import {useAppSelector} from "@/app/hooks";
 import TopicCard from "@/components/dashboard/TopicCard";
+import { selectFilteredTopics } from "@/redux/features/topics/addTopics";
 const TopicsPage = () => {
     const {setHeaderText} = useHeader()
     useEffect(()=>{
         setHeaderText("Manage all your learning topics")
     })
-    const topics = useAppSelector((state:any)=> state.topic.data)
+    const filteredTopics = useAppSelector(selectFilteredTopics);
     return (
         <div className="px-20 flex flex-col gap-6">
             <Hero title={"Topics"} description={"Manage and track all your learning topics"}/>
             <TopicFilter/>
             <div className="w-full grid grid-cols-1 mt-4 sm:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-5">
-                {topics.map((topic: any, index: number) => (
+                {filteredTopics ? filteredTopics.map((topic: any, index: number) => (
                     <TopicCard
                         key={index}
                         title={topic.title}
@@ -25,7 +26,9 @@ const TopicsPage = () => {
                         message={topic.message}
                         status={topic.status}
                     />
-                ))}
+                )): (
+                    <p>No topics found.</p>
+                )}
             </div>
         </div>
     )
