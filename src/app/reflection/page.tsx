1aace3416b82a2a5782a1be2cbc2fad946039d0f
Emirhan <emirhanerkn@hotmail.com>
2026-01-15 23:00:56 +0300

    feat: add headerText for reflection page

--- src/app/reflection/page.tsx
+++ src/app/reflection/page.tsx
@@ -1,8 +1,15 @@
+"use client"
 import Hero from "@/components/Hero/Hero"
 import AddReflection from "@/components/reflection/AddReflection";
 import PrevReflections from "@/components/reflection/PrevReflections";
 import AutoAwesomeOutlinedIcon from '@mui/icons-material/AutoAwesomeOutlined';
+import {useEffect} from "react";
+import {useHeader} from "@/app/providers";
 const ReflectionPage = () => {
+    const {setHeaderText} = useHeader();
+    useEffect(()=> {
+        setHeaderText("Reflect on your learning journey")
+    },[setHeaderText])
     return (
         <div className="flex flex-col gap-8 px-80 py-8">
         <div className="flex gap-2 items-center">
