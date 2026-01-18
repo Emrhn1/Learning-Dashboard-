007eaed4460e7fdb641e3a43e96e58ee556966a1
Emirhan <emirhanerkn@hotmail.com>
2026-01-18 22:17:01 +0300

    fix: change Hero component position

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import AddTopic from "@/components/Buttons/AddTopic"
 import {Hero} from "@/types/hero"
 const Hero = ({title,description}: Hero) => {
     return (
-        <div className="w-full mt-6 sm:mt-8 flex flex-col sm:flex-row gap-3 sm:gap-0 justify-between items-start sm:items-center">
+        <div className="w-full mb-3 sm:mt-8 flex flex-col sm:flex-row gap-3 sm:gap-0 justify-between items-start sm:items-center">
             <div className="flex flex-col gap-1">
                 <h1 className="text-lg sm:text-xl text-[#171717] font-medium">{title}</h1>
                 <span className="text-xs sm:text-sm text-[#737373]">{description}</span>
