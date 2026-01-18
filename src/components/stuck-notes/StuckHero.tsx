0069342d13fd7f4d0da42770dffff7491a3b5d63
Emirhan <emirhanerkn@hotmail.com>
2026-01-18 22:22:33 +0300

    feat: add StuckHero and design component

---
+++ src/components/stuck-notes/StuckHero.tsx
@@ -0,0 +1,21 @@
+import { Card, CardTitle, CardContent } from "@/components/ui/card"
+import {Label} from "@/components/ui/label"
+import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
+import { useAppSelector } from "@/app/hooks";
+import {selectStuckTopicsCount} from "@/redux/features/topics/addTopics";
+const StuckHero = () => {
+    const stuckCount = useAppSelector(selectStuckTopicsCount);
+    return (
+       <Card className="bg-[#FFF7ED] space-y-4">
+           <CardContent className="flex flex-row justify-between">
+               <div className="flex flex-col gap-1">
+               <Label className="text-[14px] font-normal">Topics needing attention</Label>
+               <Label className="text-[30px] font-normal">{stuckCount}</Label>
+               </div>
+               <LightbulbOutlinedIcon style={{ fontSize: "48px" }}
+                                      className="text-[#FF8904]"/>
+           </CardContent>
+       </Card>
+    )
+}
+export default StuckHero
