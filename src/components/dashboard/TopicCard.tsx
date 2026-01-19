4a53d541dcd4fd08679a10d8446d6fe0392aa061
Emirhan <emirhanerkn@hotmail.com>
2026-01-19 15:55:03 +0300

    feat: show the Progress component in TopicCard component, and set progres value if >=100 Completed if not show the other status

--- src/components/dashboard/TopicCard.tsx
+++ src/components/dashboard/TopicCard.tsx
@@ -1,50 +1,52 @@
 import {TopicCards} from "@/types/topicCard";
 import { Card, CardContent, CardDescription,CardTitle } from "@/components/ui/card"
 import {Progress} from "@/components/ui/progress";
-import {useEffect, useState} from "react";
+import { useState} from "react";
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import {levelStyles} from "@/types/topicCard";
 import {any} from "prop-types";
 import {LevelType} from "@/types/topicCard";
 import {statusStyles} from "@/types/topicCard";
 import {Status} from "@/types/topicCard";
+import {Button} from "@/components/ui/button";
 interface TopicCardProps {
     title: string;
     category: string;
     level: LevelType;
     message?: string;
     status: Status;
+    progress: number;
 }
 
-const TopicCard = ({title,category,level,status}: TopicCardProps) => {
-    const [progress, setProgress] = useState(13)
+const TopicCard = ({title,category,level,status,progress}: TopicCardProps) => {
 
-    useEffect(() => {
-        const timer = setTimeout(() => setProgress(66), 500)
-        return () => clearTimeout(timer)
-    }, [])
     const style = levelStyles[level]
     const statusStyle = statusStyles[status]
+    const [progres, setProgress] = useState(progress);
     return (
                     <Card className="w-full hover:shadow-md min-h-[185px] p-5">
                         <CardContent>
                             <div className="flex flex-col relative items-start gap-3">
                                 <CardTitle className="font-normal">{title}</CardTitle>
-                                <CardDescription className="text-[#737373]">{category}</CardDescription>
+                                <CardDescription className="text-[#737373] dark:text-sidebar-foreground">{category}</CardDescription>
                                 <div
                                     className={`px-3 mb-1 py-1 rounded-md text-xs font-medium ${style?.bg} ${style?.text} ${style?.border}`}>
                                     {level}
                                 </div>
-                                    <Progress value={progress} className="w-full"/>
-                                    <p className="text-xs sm:text-sm text-gray-500 text-right">
-                                        {progress}%
+                                    <Progress value={progres} className="w-full"/>
+                                    <p className="text-xs sm:text-sm text-gray-500 dark:text-sidebar-foreground text-right">
+                                        {progres}%
                                     </p>
+                                <div className="flex items-center gap-2">
+                                <Button className="w-4 h-7 hover:bg-gray-800 rounded-lg" disabled={progres>=100} onClick={()=> setProgress(progres+5)}>+</Button>
+                                <Button className="w-4 h-7 bg-blue-50 hover:bg-blue-100 text-black rounded-lg" disabled={progres<=0} onClick={()=> setProgress(progres-5)}>-</Button>
+                                </div>
                                 <div className="flex gap-2">
                                 <CalendarTodayIcon style={{color:"#a1a1a1"}} fontSize={"inherit"}/>
-                                <span className="text-[#a1a1a1] text-[12px]">Last Studied: Today</span>
+                                <span className="text-[#a1a1a1] dark:text-sidebar-foreground text-[12px]">Last Studied: Today</span>
                                 </div>
                                 <div className={`px-3 py-1 absolute top-0 right-0 rounded-md text-xs font-medium ${statusStyle?.bg} ${statusStyle?.text} ${statusStyle?.border}`}>
-                                    {status}
+                                    {progres >= 100 ? "Completed" : status}
                                 </div>
                             </div>
                         </CardContent>
