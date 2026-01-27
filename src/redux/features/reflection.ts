c3246899428509b4c12c4a89e21f2a8a47496ee9
Emirhan <emirhanerkn@hotmail.com>
2026-01-27 14:10:11 +0300

    feat: add id properties and add removeCard reducer

--- src/redux/features/reflection.ts
+++ src/redux/features/reflection.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export type CardsState = {
+    id:string;
   learned: string;
   difficult: string;
   createdAt: string; // Tarih ekleyelim
@@ -16,6 +17,7 @@ export const CardSlice = createSlice({
   reducers: {
     addCard: (state, action: PayloadAction<{ learned: string; difficult: string }>) => {
       state.cards.push({
+        id: Date.now().toString(),
         learned: action.payload.learned,
         difficult: action.payload.difficult,
         createdAt: new Date().toISOString(),
@@ -26,6 +28,11 @@ export const CardSlice = createSlice({
       state.cards = action.payload;
     },
 
+    removeCard: (state, action: PayloadAction<string>) => {
+      state.cards = state.cards.filter((card) => card.id !== action.payload);
+    },
+
+
     clearCards: (state) => {
       state.cards = [];
     },
@@ -33,4 +40,4 @@ export const CardSlice = createSlice({
 });
 
 export default CardSlice.reducer;
-export const { addCard, hydrateCards, clearCards } = CardSlice.actions;
+export const { addCard, hydrateCards,removeCard, clearCards } = CardSlice.actions;
